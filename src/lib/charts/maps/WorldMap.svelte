d5701da150e24e578e15a3b2268f8e7fa6920d5f
Archie <58074498+archiewood@users.noreply.github.com>
2023-06-21 14:40:36 -0400

    Update WorldMap.svelte

--- src/lib/charts/maps/WorldMap.svelte
+++ src/lib/charts/maps/WorldMap.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+    export const evidenceInclude = true;
+</script>
+
 <script>
     import CustomMapConfig from './CustomMapConfig.svelte';
     import mapJson from './worldMap.json';
